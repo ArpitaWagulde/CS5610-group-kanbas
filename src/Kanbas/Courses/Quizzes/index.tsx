3c37a2f97c4f9372a3e30c561aa9f78d8584b11e
Ananya Asthana <ananyaasthana@Ananyas-MacBook-Pro.local>
2024-04-20 20:34:42 -0400

    quiz module changes

---
+++ src/Kanbas/Courses/Quizzes/index.tsx
@@ -0,0 +1,173 @@
+import { FaCheckCircle, FaEllipsisV } from "react-icons/fa";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { FaFilePen, FaPlus } from "react-icons/fa6";
+import "./index.css";
+import { useSelector, useDispatch } from "react-redux";
+import { KanbasState } from "../../store";
+import { deleteQuiz, setQuiz, setQuizzes } from "./reducer";
+import { useEffect, useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import * as service from "./service";
+
+function getStatus(quiz:any) {
+  const currentDate = new Date();
+  const availableDate = new Date(quiz.available_date);
+  const untilDate = new Date(quiz.until_date);
+  const dueDate = new Date(quiz.due_date);
+
+  if (currentDate < availableDate) {
+      return "Not Available";
+  } else if (currentDate > untilDate) {
+      return "Not Available";
+  } else if (currentDate >= availableDate && currentDate <= untilDate) {
+      return "Available";
+  } else if (currentDate > dueDate) {
+      return "Past Due";
+  }
+}
+
+function Quizzes() {
+  const { courseId } = useParams();
+  const quizList = useSelector(
+    (state: KanbasState) => state.quizzesReducer.quizzes
+  );
+  const quiz = useSelector(
+    (state: KanbasState) => state.quizzesReducer.quiz
+  );
+  const status = getStatus(quiz);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [show, setShow] = useState(false);
+
+  const handleDeleteAssignment = (quizId: string) => {
+    service.deleteQuiz(quizId).then((status) => {
+      dispatch(deleteQuiz(quizId));
+    });
+  };
+  useEffect(() => {
+    service.findQuizzesForCourse(courseId).then((quizzes) => {
+      dispatch(setQuizzes(quizzes));
+    });
+  }, [courseId]);
+
+  return (
+    <div className="flex-fill">
+      <Modal show={show} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Quiz</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to delete this quiz?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="primary"
+            onClick={() => {
+              handleDeleteAssignment(quiz._id);
+              setShow(false);
+            }}
+          >
+            Ok
+          </Button>
+          <Button variant="secondary" onClick={() => setShow(false)}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <div
+        style={{ justifySelf: "stretch" }}
+        className="d-flex wd-asmt-home-buttons justify-content-between m-2"
+      >
+        <input
+          type="text"
+          className="form-control w-25"
+          placeholder="Search for Quiz"
+        />
+        <div style={{ justifyContent: "flex-end" }}>
+          <button type="button" className="btn btn-light">
+            <FaPlus className="ms-2" /> Group
+          </button>
+          <button
+            type="button"
+            className="btn btn-light"
+            style={{ backgroundColor: "red", color: "white" }}
+            onClick={() => {
+              navigate(
+                `/Kanbas/Courses/${courseId}/Quizzes/${
+                  "A" + new Date().getTime().toString()
+                }`
+              );
+            }}
+          >
+            <FaPlus className="ms-2" /> Quiz
+          </button>
+          <button
+            style={{ height: "37px" }}
+            type="button"
+            className="btn btn-light"
+          >
+            <FaEllipsisV />
+          </button>
+          <br />
+        </div>
+      </div>
+      <hr className="ms-2 me-2" />
+      <ul className="list-group wd-asmt-list m-2">
+        <li className="list-group-item">
+          <div>
+            <FaEllipsisV className="me-2" /> QUIZZES
+            <span className="float-end">
+              <span className="p-1 me-2 badge border border-dark rounded-pill text-secondary">
+                40% of Total
+              </span>
+              <FaPlus className="ms-2" />
+              <FaEllipsisV className="ms-2" />
+            </span>
+          </div>
+          <ul className="list-group">
+            {quizList.map((quiz) => (
+              <li className="list-group-item">
+                <div className="d-flex">
+                  <div style={{ alignSelf: "center" }}>
+                    <FaEllipsisV className="me-2" />
+                    <FaFilePen className="m-2 text-success" />
+                  </div>
+                  <div className="text-secondary p-1">
+                    <Link
+                      to={`/Kanbas/Courses/${courseId}/Quizzes/${quiz._id}`}
+                    >
+                      {quiz.title}
+                    </Link>
+                    <br />
+                    <small>
+                      {status} | Due {quiz.due_date} |{" "}
+                      {quiz.points} 
+                    </small>
+                  </div>
+                  <div className="ms-auto" style={{ alignSelf: "center" }}>
+                    <span>
+                      <button
+                        onClick={() => {
+                          dispatch(setQuiz(quiz));
+                          setShow(true);
+                        }}
+                        className="btn btn-danger m-2 p-1"
+                        style={{ borderRadius: "0.375rem" }}
+                      >
+                        Delete
+                      </button>
+                      <FaCheckCircle className="text-success" />
+                      <FaEllipsisV className="ms-2" />
+                    </span>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
+}
+export default Quizzes;
---
+++ src/Kanbas/Courses/Quizzes/reducer.ts
@@ -0,0 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  quizzes: <any[]>[],
+  quiz: {
+    status: "Closed",
+    due_date: "2024-03-04",
+    points: "8 pts",
+    question_count: "12 Questions"
+  },
+};
+
+const quizzesSlice = createSlice({
+  name: "quizzes",
+  initialState,
+  reducers: {
+    addQuiz: (state, action) => {
+      state.quizzes = [{ ...action.payload }, ...state.quizzes];
+    },
+
+    deleteQuiz: (state, action) => {
+      state.quizzes = state.quizzes.filter(
+        (quiz) => quiz._id !== action.payload
+      );
+    },
+
+    updateQuiz: (state, action) => {
+      state.quizzes = state.quizzes.map((quiz) => {
+        if (quiz._id === action.payload._id) {
+          return action.payload;
+        } else {
+          return quiz;
+        }
+      });
+    },
+    setQuiz: (state, action) => {
+      state.quiz = action.payload;
+    },
+    setQuizzes: (state, action) => {
+      state.quizzes = action.payload;
+    },
+  },
+});
+
+export const {
+  addQuiz,
+  deleteQuiz,
+  updateQuiz,
+  setQuiz,
+  setQuizzes,
+} = quizzesSlice.actions;
+
+export default quizzesSlice.reducer;
