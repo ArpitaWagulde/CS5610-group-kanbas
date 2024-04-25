56383072390c547f9248a87fbca109c437bfa8f2
Ananya Asthana <ananyaasthana@Ananyas-MacBook-Pro.local>
2024-04-25 17:52:49 -0400

    preview screen

--- src/Kanbas/Courses/Quizzes/Preview/index.tsx
+++ src/Kanbas/Courses/Quizzes/Preview/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import "./index.css";
 import { setQuiz } from "../reducer";
 import { setQuestions } from "../Editor/reducer";
@@ -12,8 +12,12 @@ import { FaInfoCircle } from "react-icons/fa";
 function Preview() {
   const { quizId } = useParams();
   const dispatch = useDispatch();
-
+  const navigate = useNavigate();
+  const questions = useSelector(
+    (state: KanbasState) => state.questionsReducer.questions
+  );
   const quiz = useSelector((state: KanbasState) => state.quizzesReducer.quiz);
+
   useEffect(() => {
     service.findQuiz(quizId).then((quiz) => {
       dispatch(setQuiz(quiz));
@@ -22,7 +26,36 @@ function Preview() {
       dispatch(setQuestions(questions));
     });
   }, [quizId]);
-  // console.log(questions);
+
+  const renderOptions = (questionType: string, options: string[]) => {
+    switch (questionType) {
+      case "FillinTheBlanks":
+        return (
+        <div>
+          Answer: <input type="text" />
+          </div>);
+      case "TrueorFalse":
+        return (
+          <div>
+            <input type="radio" name="trueFalse" value="true" />
+            <label>True</label>
+            <br></br>
+            <input type="radio" name="trueFalse" value="false" />
+            <label>False</label>
+          </div>
+        );
+      case "MultipleChoice":
+        return options.map((option, index) => (
+          <div key={index}>
+            <input type="radio" name="multipleChoice" value={option} />
+            <label>{option}</label>
+          </div>
+        ));
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="wd-asmt-edit-home flex-fill">
       <h2>{quiz?.title}</h2>
@@ -34,6 +67,31 @@ function Preview() {
           This is a preview of the published version of the quiz
         </div>
       </div>
+      <br />
+      <br />
+      <br />
+      <h2>Quiz Instructions</h2>
+      <hr />
+      <div>
+        {questions.map((question, index) => (
+          <div key={index}>
+            <div className="card">
+              <div className="card-header bg-light-grey">
+                <h2 className="card-heading">{question.title}</h2>
+              </div>
+              <div className="card-body">
+                {question.description}
+                <hr></hr>
+                {renderOptions(question.type, question.options)}
+              </div>
+            </div>
+            <br />
+          </div>
+        ))}
+      </div>
+      <button className="submit-quiz-btn">
+        Submit Quiz
+      </button>
     </div>
   );
 }
